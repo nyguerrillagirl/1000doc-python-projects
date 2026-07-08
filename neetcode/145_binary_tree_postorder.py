7f045ad64d607822e515425fd0054b4f02bd6fd0
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2026-07-08 07:18:09 -0400

    "Day #648 - processing the array -> tree correctly now"

--- neetcode/145_binary_tree_postorder.py
+++ neetcode/145_binary_tree_postorder.py
@@ -10,30 +10,37 @@
 # Output: [3, 2, 1]
 
 # NOT WORKING
+from collections import deque
 
-
-def post_order_traversal(tree, root_index):
+def post_order_traversal(tree):
     result = []
-    if root_index >= len(tree):
+    if len(tree) == 0:
         return result
-
-    # process right sub-tree (if it exists)
-    left_child_index = 2 * root_index + 1
-    right_child_index = 2 * root_index + 2
-
-    if (left_child_index < len(tree)) and (tree[left_child_index] is not None):
-        result.extend(post_order_traversal(tree, left_child_index))
-
-    if (right_child_index < len(tree)) and (tree[right_child_index] is not None):
-        result.extend(post_order_traversal(tree, right_child_index))
-
-    # now add this root to the result
-    result.append(tree[root_index])
-
+    dq = deque([0])
+    adjusted_null_index = 0
+    while len(dq) != 0:
+        # extract left-most item
+        current_index = dq.popleft()
+        if tree[current_index] is None:
+            adjusted_null_index += 1
+        else:
+            print(f"root: {tree[current_index]}")
+            left_child_index = 2 * (current_index - adjusted_null_index) + 1
+            right_child_index = 2 * (current_index - adjusted_null_index) + 2
+            if left_child_index < len(tree):
+                print(f"\tleft_child: {tree[left_child_index]}")
+                dq.append(left_child_index)
+            else:
+                print(f"\tleft_child: None")
+            if right_child_index < len(tree):
+                print(f"\tright_child: {tree[right_child_index]}")
+                dq.append(right_child_index)
+            else:
+                print(f"\tright_child: None")
     return result
 
 
 if __name__ == '__main__':
     x = [1,2,3,4,5,None,8,None,None,6,7,9]
     # Output: [4,6,7,5,2,9,8,3,1]
-    print(f"x={x} is {post_order_traversal(x, 0)}.")
+    print(f"x={x} is {post_order_traversal(x)}.")
