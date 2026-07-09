5c8bf1266d488e0610365a0de7a3f8c3e2ec1228
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2026-07-09 08:54:53 -0400

    "Day #649 - Neetcode problem 145 - binary tree postorder traversal"

--- neetcode/145_binary_tree_postorder.py
+++ neetcode/145_binary_tree_postorder.py
@@ -9,10 +9,11 @@
 # Input: root = [1, null, 2, 3]
 # Output: [3, 2, 1]
 
-# NOT WORKING
+# binary tree postorder traveral is left-right-root order
 from collections import deque
 
-def post_order_traversal(tree):
+
+def print_tree(tree):
     result = []
     if len(tree) == 0:
         return result
@@ -40,7 +41,49 @@ def post_order_traversal(tree):
     return result
 
 
+def post_order_traversal(tree):
+    result = []  # holds the return value
+    # state_of_node - 'not_processed', 'processed'
+    adjusted_null_index = 0
+    null_index_count = []
+    for i in range(len(tree)):
+        if tree[i] is None:
+            adjusted_null_index += 1
+        null_index_count.append(adjusted_null_index)
+
+    print(f"null_index_count: {null_index_count}")
+
+    index_of_nodes_to_process = [(0, 'not_processed', tree[0])]  # hold tuple (index_in_tree, state_of_node)
+    while len(index_of_nodes_to_process) != 0:
+        # pop the stack
+        t = index_of_nodes_to_process.pop()
+        current_index_in_tree = t[0]
+        if t[2] is None:
+            adjusted_null_index = adjusted_null_index + 1
+            continue
+
+        if t[1] == 'not_processed':
+            # put it back on the stack
+            index_of_nodes_to_process.append((t[0], 'processed', t[2]))
+        else:
+            # we must have processed the left and right subtrees for this node already
+            result.append(tree[t[0]])
+            continue
+
+        # find out left and right children and put on stack as unprocessed (right first) (left second)
+        left_child_index = 2 * (current_index_in_tree - null_index_count[t[0]]) + 1
+        right_child_index = 2 * (current_index_in_tree - null_index_count[t[0]]) + 2
+        if 0 <= right_child_index < len(tree):
+            index_of_nodes_to_process.append((right_child_index, 'not_processed', tree[right_child_index]))
+
+        if 0 <= left_child_index < len(tree):
+            index_of_nodes_to_process.append((left_child_index, 'not_processed', tree[left_child_index]))
+
+
+    return result
+
+
 if __name__ == '__main__':
-    x = [1,2,3,4,5,None,8,None,None,6,7,9]
+    x = [1, 2, 3, 4, 5, None, 8, None, None, 6, 7, 9]
     # Output: [4,6,7,5,2,9,8,3,1]
     print(f"x={x} is {post_order_traversal(x)}.")
